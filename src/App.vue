6f4e3a703ba5ebfdea578f3c772f5aad424d2507
Jeremy9714 <1822550890@qq.com>
2025-02-08 11:22:22 +0800

    vue配置代理devServer

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,36 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <TheHeader @addTodo="addTodo"></TheHeader>
-        <TheList :todos="todos"></TheList>
-        <TheFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearAllTodo="clearAllTodo"></TheFooter>
-      </div>
-    </div>
+  <div>
+    <button @click="sendRequest">发送请求</button>
   </div>
 </template>
 
 <script>
-import TheHeader from "@/components/TheHeader";
-import TheList from "@/components/TheList";
-import TheFooter from '@/components/TheFooter'
+
+import axios from 'axios'
 
 export default {
   name: "App",
-  components: {
-    TheHeader,
-    TheList,
-    TheFooter
-  },
-  data() {
-    return {
-      todos: JSON.parse(localStorage.getItem('todos')) || []
-    }
-  },
+  components: {},
   methods: {
-    addTodo(todoObj) {
-      this.todos.unshift(todoObj)
-    },
-    checkTodo(id) {
-      const todo = this.todos.find(todoObj => todoObj.id === id)
-      todo.completed = !todo.completed
-    },
-    updateTodo(id, title) {
-      this.todos.forEach((todoObj) => {
-        if (todoObj.id === id) todoObj.title = title
-      })
-    },
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todoObj => todoObj.id !== id)
-    },
-    checkAllTodo(completed) {
-      this.todos.forEach(todoObj => todoObj.completed = completed)
-    },
-    clearAllTodo() {
-      this.todos = this.todos.filter(todoObj => !todoObj.completed)
+    sendRequest() {
+      // axios.get('http://localhost:9999/backend-mock/restapi/v1/test2').then(
+      //     response => {
+      //       console.log('请求成功！', response.data)
+      //     },
+      //     error => {
+      //       console.log('请求失败！', error.message)
+      //     }
+      // )
+
+      // axios.post('http://localhost:9999/backend-mock/restapi/v1/test', '')
+      // axios.post('http://localhost:8080/backend-mock/restapi/v1/test', '')
+      axios.post('http://localhost:8080/backend/backend-mock/restapi/v1/test', '')
+          .then(response => console.log('请求成功！', response.data))
+          .catch(error => console.log('请求失败！', error.message));
     }
-  },
-  watch: {
-    todos: {
-      deep: true,
-      handler(newVal) {
-        localStorage.setItem('todos', JSON.stringify(newVal))
-      }
-    }
-  },
-  mounted() {
-    this.$bus.$on('checkTodo', this.checkTodo)
-    this.$bus.$on('deleteTodo', this.deleteTodo)
-    this.$bus.$on('updateTodo', this.updateTodo)
-  },
-  beforeDestroy() {
-    this.$bus.$off('checkTodo')
-    this.$bus.$off('deleteTodo')
-    this.$bus.$off('updateTodo')
   }
 }
 </script>
 
 <style>
-/*base*/
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn-edit {
-  color: #fff;
-  background-color: skyblue;
-  border: 1px solid rgb(103, 159, 180);
-  margin-right: 5px;
-}
-
-.btn-edit:hover {
-  color: #fff;
-  background-color: dodgerblue;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    // 关闭语法检查
     lintOnSave: false,
+    // 开启代理服务器
+    devServer: {
+        proxy: {
+            '/backend': {// 代理接口前缀为/backend的请求
+                target: 'http://localhost:9999', // 对应的代理地址
+                secure: false, // 接受运行在https上，默认不接受
+                changeOrigin: true, // 如果设置为true,那么本地会虚拟一个服务器接收你的请求并代你发送该请求,这样就不会有跨域问题（只适合开发环境）
+                pathRewrite: {'^/backend': ''} //重写路径 比如'/backend/aaa/ccc'重写为'/aaa/ccc'
+            }
+        }
+    }
 })
